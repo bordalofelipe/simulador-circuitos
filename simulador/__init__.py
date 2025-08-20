b55a7838c2f939dce9c9039b0e1d63b5ff757519
Felipe Bordalo <felipebordalo.20221@poli.ufrj.br>
2025-08-19 22:18:01 -0300

    Initial code structure

---
+++ simulador/__init__.py
@@ -0,0 +1,165 @@
+from simulador.componentes import *
+
+class Circuito():
+    def __init__(self, simulacao: str, tempo_total: float, passo: float, tipo_simulacao: str, passo_interno: int):
+        self.simulacao = simulacao
+        self.tempo_total = tempo_total
+        self.tipo_simulacao = tipo_simulacao
+        self.passo = passo
+        self.passo_interno = passo_interno
+        # define algumas coisas internas
+        self.__componentes: list[Componente] = []
+        self.__nos = []
+
+    def __iadd__(self, componente):
+        if (isinstance(componente, Componente)):
+            self.__componentes.append(componente)
+    
+    def __setitem__(self, index, componente):
+        if (isinstance(componente, Componente)):
+            self.__componentes[index] = componente
+
+    def __getitem__(self, index):
+        return self.__componentes[index]
+    
+    def __delitem__(self, index):
+        return self.__componentes.pop(index)
+    
+    def __len__(self):
+        return len(self.__componentes)
+    
+    def append(self, componente):
+        return self.__iadd__(componente)
+    
+    def remove(self, componente):
+        return self.__componentes.__delitem__(componente)
+    
+    def pop(self, index):
+        return self.__delitem__(index)
+
+    def __popular_nos(self):
+        self.__nos = []
+        for comp in self.__componentes:
+            nos = comp.nos
+            for no in nos:
+                if no not in self.__nos:
+                    self.__nos.append(no)
+
+    def run(self):
+        self.__popular_nos()
+        r = Resultado(self.__nos, [], [])
+        for i in range(10000):
+            r.append(i, [1]*len(self.__nos))
+        return r
+    
+    def export(self, filename: str):
+        with open(filename, 'w') as f:
+            self.__popular_nos()
+            f.write(str(len(self.__nos)-1) + '\n') # por causa do no terra, tirar 1
+            for com in self.__componentes:
+                f.write(str(com) + '\n')
+            f.write(self.simulacao + ' ' + str(self.tempo_total) + ' ' + str(self.passo) + ' ' + self.tipo_simulacao + ' ' + str(self.passo_interno))
+
+def import_netlist(filename: str):
+    with open(filename) as f:
+        line = f.readline() # nao usamos primeira linha
+        componentes = []
+        line = f.readline()
+        while line != '':
+            line = line.replace('\n', '')
+            tipo = line[0]
+            c = line.split(' ')
+            if tipo == 'R':
+                componentes.append(Resistor(c[0][1:], [c[1], c[2]], float(c[3])))
+            elif tipo == 'L':
+                if len(c) == 5:
+                    ic = c[4].replace('IC=', '')
+                    componentes.append(Indutor(c[0][1:], [c[1], c[2]], float(c[3]), float(ic)))
+                else:
+                    componentes.append(Indutor(c[0][1:], [c[1], c[2]], float(c[3])))
+            elif tipo == 'C':
+                if len(c) == 5:
+                    ic = c[4].replace('IC=', '')
+                    componentes.append(Capacitor(c[0][1:], [c[1], c[2]], float(c[3]), float(ic)))
+                else:
+                    componentes.append(Capacitor(c[0][1:], [c[1], c[2]], float(c[3])))
+            elif tipo == 'E':
+                componentes.append(FonteTensaoTensao(c[0][1:], [c[1], c[2], c[3], c[4]], float(c[5])))
+            elif tipo == 'F':
+                componentes.append(FonteCorrenteCorrente(c[0][1:], [c[1], c[2], c[3], c[4]], float(c[5])))
+            elif tipo == 'G':
+                componentes.append(FonteCorrenteTensao(c[0][1:], [c[1], c[2], c[3], c[4]], float(c[5])))
+            elif tipo == 'H':
+                componentes.append(FonteTensaoCorrente(c[0][1:], [c[1], c[2], c[3], c[4]], float(c[5])))
+            elif tipo == 'O':
+                componentes.append(AmpOp(c[0][1:], [c[1], c[2], c[3]]))
+            elif tipo == 'D':
+                componentes.append(Diodo(c[0][1:], [c[1], c[2]]))
+            elif tipo == 'M':
+                raise NotImplementedError
+                componentes.append(Mosfet(c[0][1:], [c[1], c[2], c[3]], c[4], c[5], c[6], c[7], c[8], c[9]))
+            elif tipo == 'I':
+                componentes.append(FonteCorrente(c[0][1:], [c[1], c[2]], c[2:]))
+            elif tipo == 'V':
+                componentes.append(FonteTensao(c[0][1:], [c[1], c[2]], c[2:]))
+            elif tipo == '.':
+                # arquivo acabou!
+                break
+            line = f.readline()
+        params = line.split(' ')
+        circuit = Circuito(params[0], float(params[1]), float(params[2]), params[3], int(params[4]))
+        for comp in componentes:
+            circuit.append(comp)
+        return circuit
+    return Circuito()
+
+class Resultado():
+    def __init__(self, nos: list[str], t: list[float], resultado: list[list[float]]):
+        self.__nos = nos
+        self.__t = t
+        self.__resultado = resultado
+    
+    def append(self, t: float, resultado: list[float]):
+        assert len(resultado) == len(self.__nos)
+        self.__t.append(t)
+        self.__resultado.append(resultado)
+    
+    def export(self, filename: str):
+        with open(filename, 'w') as f:
+            f.write('t ')
+            f.write(' '.join(self.__nos))
+            f.write('\n')
+            for t, r in zip(self.__t, self.__resultado):
+                f.write(str(t) + ' ')
+                f.write(' '.join(str(i) for i in r))
+                f.write('\n')
+        return self.__resultado
+    
+    def to_numpy(self):
+        return False
+
+    def to_pandas(self):
+        return False
+    
+def import_resultado(filename: str):
+    with open(filename) as f:
+        line = f.readline()
+        nos = line.split(';')[1:]
+        resultado = Resultado(nos, [], [])
+        line = f.readline()
+        while line != '':
+            r = line.split(';')
+            r = [float(i) for i in r]
+            t, r = r[0], r[1:]
+            resultado.append(t, r)
+            line = f.readline()
+        return resultado
+
+
+if __name__ == '__main__':
+    c = import_netlist('entrada.txt')
+    resultado = c.run()
+    resultado.export('saida.txt')
+else:
+    # nao faz nada
+    pass
---
+++ simulador/componentes.py
@@ -0,0 +1,265 @@
+class Componente():
+    _linear = True
+    _num_nos = 0
+    _num_nos_mod = 0
+    def __init__(self, name: str, nos: list[str]):
+        '''Classe abstrata de componente'''
+        self._nos_mod = []
+        assert len(nos) == self._num_nos
+        self.name = name
+        self.nos = nos
+
+    @property
+    def linear(self):
+        '''Retorna se componente é linear'''
+        return self._linear
+
+    @property
+    def num_nos_mod(self):
+        '''Retorna numero de nos necessarios na analise modificada'''
+        return self._num_nos_mod
+    
+    def set_nos_mod(self, nos_mod: list[int]):
+        '''Aloca nos necessarios na analise modificada'''
+        assert len(nos_mod) == self._num_nos_mod
+        self._nos_mod = nos_mod
+    
+    def set_posicao_nos(self, posicoes: list[int]):
+        '''Aloca nos necessarios'''
+        self._posicao_nos = posicoes
+    
+    def __str__(self):
+        return 'Componente' 
+
+    def estampaDC(self, Gn, I):
+        raise NotImplementedError
+    def estampaBE(self, Gn, I):
+        raise NotImplementedError
+    def estampaTrap(self, Gn, I):
+        raise NotImplementedError
+    def estampaFE(self, Gn, I):
+        raise NotImplementedError
+
+class Resistor(Componente):
+    _linear = True
+    _num_nos = 2
+    _num_nos_mod = 0
+    def __init__(self, name: str, nos: list[str], valor: float):
+        '''
+        Resistor
+        nos: [no1, no2] nos do Resistor
+        valor: resistencia
+        '''
+        super().__init__(name, nos)
+        self.valor = valor
+    
+    def __str__(self):
+        return 'R' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+class Indutor(Componente):
+    _linear = True
+    _num_nos = 2
+    _num_nos_mod = 0
+    def __init__(self, name: str, nos: list[str], valor: float, ic=0.0):
+        '''
+        Indutor
+        nos: [no_mais, no_menos] nos do Indutor
+        valor: indutancia
+        ic = 0.0: valor inicial
+        '''
+        super().__init__(name, nos)
+        self.valor = valor
+        self.ic = ic
+
+    def __str__(self):
+        if self.ic == 0.0:
+            return 'I' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor)
+        else:
+            return 'I' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor) + ' IC=' + str(self.ic)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+class Capacitor(Componente):
+    _linear = True
+    _num_nos = 2
+    _num_nos_mod = 0
+    def __init__(self, name: str, nos: list[str], valor: float, ic=0.0):
+        '''
+        Capacitor
+        nos: [no_mais, no_menos] nos do Capacitor
+        valor: capacitancia
+        ic = 0.0: valor inicial
+        '''
+        super().__init__(name, nos)
+        self.valor = valor
+        self.ic = ic
+
+    def __str__(self):
+        if self.ic == 0.0:
+            return 'C' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor)
+        else:
+            return 'C' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor) + ' IC=' + str(self.ic)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+# tensao controlada por tensao
+class FonteTensaoTensao(Componente):
+    _linear = True
+    _num_nos = 4
+    _num_nos_mod = 1
+    def __init__(self, name: str, nos: list[str], valor: float):
+        '''
+        Fonte de tensao controlada por tensao
+        nos: [no_mais, no_menos] nos da fonte
+        valor: ganho
+        '''
+        super().__init__(name, nos)
+        self.valor = valor
+    
+    def __str__(self):
+        return 'F' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+# corrente controlada por corrente
+class FonteCorrenteCorrente(Componente):
+    _linear = True
+    _num_nos = 4
+    _num_nos_mod = 1
+    def __init__(self, name: str, nos: list[str], valor: float):
+        '''
+        Fonte de corrente controlada por corrente
+        nos: [no_mais, no_menos] nos da fonte
+        valor: ganho
+        '''
+        super().__init__(name, nos)
+        self.valor = valor
+    
+    def __str__(self):
+        return 'G' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+# corrente controlada por tensao
+class FonteCorrenteTensao(Componente):
+    _linear = True
+    _num_nos = 4
+    _num_nos_mod = 0
+    def __init__(self, name: str, nos: list[str], valor: float):
+        '''
+        Fonte de corrente controlada por tensao
+        nos: [no_mais, no_menos] nos da fonte
+        valor: ganho
+        '''
+        super().__init__(name, nos)
+        self.valor = valor
+    
+    def __str__(self):
+        return 'G' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+# tensao controlada por corrente
+class FonteTensaoCorrente(Componente):
+    _linear = True
+    _num_nos = 4
+    _num_nos_mod = 2
+    def __init__(self, name: str, nos: list[str], valor: float):
+        '''
+        Fonte de tensao controlada por corrente
+        nos: [no_mais, no_menos] nos da fonte
+        valor: ganho
+        '''
+        super().__init__(name, nos)
+        self.valor = valor
+    
+    def __str__(self):
+        return 'H' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + str(self.valor)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+class Diodo(Componente):
+    _linear = True
+    _num_nos = 2
+    _num_nos_mod = 2
+    def __init__(self, name: str, nos: list[str]):
+        '''
+        Diodo
+        nos: [no_mais, no_menos] nos do diodo
+        '''
+        super().__init__(name, nos)
+
+    def __str__(self):
+        return 'D' + self.name + ' ' + ' '.join(str(no) for no in self.nos)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+class AmpOp(Componente):
+    _linear = True
+    _num_nos = 3
+    _num_nos_mod = 1
+    def __init__(self, name: str, nos: list[str]):
+        '''
+        Amplificador Operacional
+        nos: [no_mais, no_menos, no_saida] nos do amp op
+        '''
+        super().__init__(name, nos)
+        
+    def __str__(self):
+        return 'O' + self.name + ' ' + ' '.join(str(no) for no in self.nos)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+class Mosfet(Componente):
+    def __init__(self):
+        '''Mosfet'''
+        raise NotImplementedError
+
+class FonteCorrente(Componente):
+    _linear = True
+    _num_nos = 2
+    _num_nos_mod = 0
+    def __init__(self, name: str, nos: list[str], args: list):
+        '''
+        Fonte de corrente
+        nos: [no_mais, no_menos] nos da fonte
+        list: parametros no estilo SPICE
+        '''
+        super().__init__(name, nos)
+        self.args = args
+    
+    def __str__(self):
+        return 'I' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + ' '.join(self.args)
+    
+    def estampaDC(self, Gn, I):
+        pass
+
+class FonteTensao(Componente):
+    _linear = True
+    _num_nos = 2
+    _num_nos_mod = 1
+    def __init__(self, name: str, nos: list[str], args: list):
+        '''
+        Fonte de tensao
+        nos: [no_mais, no_menos] nos da fonte
+        list: parametros no estilo SPICE
+        '''
+        super().__init__(name, nos)
+        self.args = args
+        
+    def __str__(self):
+        return 'V' + self.name + ' ' + ' '.join(str(no) for no in self.nos) + ' ' + ' '.join(self.args)
+    
+    def estampaDC(self, Gn, I):
+        pass
